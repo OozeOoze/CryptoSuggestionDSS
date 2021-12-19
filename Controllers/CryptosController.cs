8b91ade6535d6c20ac83e403b98da240456360bc
OguzMulti <oguz@multiplayer.com.tr>
2021-12-19 16:17:30 +0300

    Add project files.

---
+++ Controllers/CryptosController.cs
@@ -0,0 +1,55 @@
+﻿using CryptoSuggestion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CryptoSuggestion.Controllers
+{
+    public class CryptosController : Controller
+    {
+        private readonly IHttpClientFactory _clientfactory;
+
+        public CoinMarketData CoinMarketData { get; set; }
+
+        const string BASE_URL = "https://pro-api.coinmarketcap.com";
+        const string API_KEY = "a1d7197f-5ba9-4e9a-94f6-692628e4278e";
+
+        public CryptosController(IHttpClientFactory clientFactory)
+        {
+            _clientfactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri($"{BASE_URL}/v1/cryptocurrency/listings/latest");
+            message.Headers.Add("Accepts","application/json");
+            message.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+
+            var client = _clientfactory.CreateClient();
+
+            var response = await client.SendAsync(message);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+
+
+
+                CoinMarketData = await JsonSerializer.DeserializeAsync<CoinMarketData>(responseStream);
+            }
+            else
+            {
+                CoinMarketData = null;
+            }
+
+            return View(CoinMarketData);
+        }
+    }
+}
---
+++ Models/CoinMarketData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoSuggestion.Models
+{
+    public class CoinMarketData
+    {
+        public Status status { get; set; }
+        public Datum[] data { get; set; }
+    }
+
+    public class Status
+    {
+        public DateTime timestamp { get; set; }
+        public int error_code { get; set; }
+        public object error_message { get; set; }
+        public int elapsed { get; set; }
+        public int credit_count { get; set; }
+        public object notice { get; set; }
+        public int total_count { get; set; }
+    }
+
+    public class Datum
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public string slug { get; set; }
+        public int num_market_pairs { get; set; }
+        public DateTime date_added { get; set; }
+        public string[] tags { get; set; }
+        public long? max_supply { get; set; }
+        public float circulating_supply { get; set; }
+        public float total_supply { get; set; }
+        public Platform platform { get; set; }
+        public int cmc_rank { get; set; }
+        public DateTime last_updated { get; set; }
+        public Quote quote { get; set; }
+    }
+
+    public class Platform
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public string slug { get; set; }
+        public string token_address { get; set; }
+    }
+
+    public class Quote
+    {
+        public USD USD { get; set; }
+    }
+
+    public class USD
+    {
+        public float price { get; set; }
+        public float volume_24h { get; set; }
+        public float volume_change_24h { get; set; }
+        public float percent_change_1h { get; set; }
+        public float percent_change_24h { get; set; }
+        public float percent_change_7d { get; set; }
+        public float percent_change_30d { get; set; }
+        public float percent_change_60d { get; set; }
+        public float percent_change_90d { get; set; }
+        public float market_cap { get; set; }
+        public float market_cap_dominance { get; set; }
+        public float fully_diluted_market_cap { get; set; }
+        public DateTime last_updated { get; set; }
+    }
+}
